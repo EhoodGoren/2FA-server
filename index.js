ab9bec719ad3819ae6be19608dccb254878e2283
EhoodGoren <ehoodgoren@gmail.com>
2021-12-12 22:42:36 +0200

    Basic setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('hello');
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
